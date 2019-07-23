700ff441b232d51899c5b918af46c7028e8adadb
etying <wjy@xchainet.com>
2019-07-23 00:03:48 -0700

    modify node/preset.sh sleep 30

--- bdd/node/preset.sh
+++ bdd/node/preset.sh
@@ -59,7 +59,7 @@ sleep 1
 sed -i 's/"mediator_interval": 3,/"mediator_interval": 2,/g' node1/ptn-genesis.json
 sed -i 's/"maintenance_skip_slots": 1,/"maintenance_skip_slots": 0,/g' node1/ptn-genesis.json
 ./start.sh
-sleep 1
+sleep 20
 numcommand=`ps -ef|grep gptn |wc -l`
 num=`echo $numcommand | sed -n '$p'| awk '{print $NF}'`
 
