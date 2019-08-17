a43b3811d9dfe944093a37d1a6f27ea9890b974c
shuizhongmose <602554102@qq.com>
2019-08-17 08:49:11 +0800

    modified:   ../../common/crypto/crypto.go

--- common/crypto/crypto.go
+++ common/crypto/crypto.go
@@ -8,7 +8,7 @@
 //
 // The go-ethereum library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// MERCHANTABILITY or FITNESS FOR A PARTCryptoS256ICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
@@ -39,7 +39,7 @@ var (
 	secp256k1_N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
 	secp256k1_halfN = new(big.Int).Div(secp256k1_N, big.NewInt(2))
 )
-var MyCryptoLib ICrypto = &CryptoP256{}
+var MyCryptoLib ICrypto = &CryptoS256{}
 
 // Keccak256 calculates and returns the Keccak256 hash of the input data.
 func Keccak256(data ...[]byte) []byte {
