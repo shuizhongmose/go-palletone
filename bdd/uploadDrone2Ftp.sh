3159b0e91f0cc54571f0a3b2855556c40ac51169
shuizhongmose <602554102@qq.com>
2019-09-03 16:34:43 +0800

    add upload bash shell

---
+++ bdd/uploadDrone2Ftp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function obtain_git_branch {
+  br=`git branch | grep "*"`
+  echo ${br/* /}
+}
+
+function obtain_git_commit_number {
+  br=`git rev-list --count HEAD`
+  echo ${br/ /}
+}
+
+folder=`obtain_git_branch`
+number=`obtain_git_commit_number`
+
+set timeout 120
+set ftppwd [lindex $argv 0]
+spawn lftp travis:$ftppwd@47.74.209.46 << EOF
+cd ${folder}
+mkdir ${number}
+cd ${number}
+mirror -R /drone/src/github.com/palletone/go-palletone/bdd/logs
+exit
+EOF
