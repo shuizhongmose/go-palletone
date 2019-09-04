88ead5d78a1b63a128837bb9059f6619edd7bf23
shuizhongmose <602554102@qq.com>
2019-09-04 16:37:00 +0800

    fix matrix

---
+++ bdd/Digital-Identity/scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $GOPATH/src/github.com/
+cd palletone
+git clone https://github.com/palletone/digital-identity.git
+cd $BASE_DIR/bdd/Digital-Identity/scripts
+chmod +x ca-start.sh
+chmod +x one-ptn.sh
+./ca-start.sh
+tree ~/cawork
+./one-ptn.sh
+sleep 30
+netstat -ntl
+cd $BASE_DIR
+mkdir -p ${BDD_LOG_PATH}/${DIGITAL_IDENTITY_DIR}
+python -m robot.run -d ${BDD_LOG_PATH}/${DIGITAL_IDENTITY_DIR} ./bdd/Digital-Identity/testcases
