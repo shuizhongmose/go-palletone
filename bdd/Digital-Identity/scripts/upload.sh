e3ac76ed2e9f5dbca439c02fa2bfaceb410ec2f6
shuizhongmose <602554102@qq.com>
2019-09-05 10:00:14 +0800

    add log

--- bdd/Digital-Identity/scripts/upload.sh
+++ bdd/Digital-Identity/scripts/upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /drone/src/github.com/palletone/go-palletone &&
-cp bdd/Digital-Identity/node/log/all.log $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR &&
-cp bdd/Digital-Identity/node/nohup.out $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR &&
+cd /drone/src/github.com/palletone/go-palletone
+cp bdd/Digital-Identity/node/log/all.log $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
+cp bdd/Digital-Identity/node/nohup.out $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
+ls $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
 ./bdd/uploadDrone2Ftp.sh $FTP_PWD
