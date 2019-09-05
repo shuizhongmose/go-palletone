b48a3e9e229b04c96e19748dfc41e63ef9da7a38
shuizhongmose <602554102@qq.com>
2019-09-05 14:40:35 +0800

    fix upload 530 error

--- bdd/uploadDrone2Ftp.sh
+++ bdd/uploadDrone2Ftp.sh
@@ -13,14 +13,18 @@ function obtain_git_commit_number {
 
 folder=`obtain_git_branch`
 number=`obtain_git_commit_number`
+
 set timeout 120
-#set ftppwd [lindex $argv 1]
-ftppwd=$1
-echo $ftppwd
-lftp travis:$ftppwd@47.74.209.46 << EOF
-cd $folder
-mkdir $number
-cd $number
-mirror -R /drone/src/github.com/palletone/go-palletone/bdd/logs
-exit
-EOF
+set ftppwd [lindex $argv 0]
+spawn lftp travis:$ftppwd@47.74.209.46
+expect "lftp"
+send "cd ${folder}\n"
+expect "cd"
+send "mkdir ${number}\n"
+expect "mkdir"
+send "cd ${number}\n"
+expect "cd"
+send "mirror -R /home/travis/gopath/src/github.com/palletone/go-palletone/bdd/logs\n"
+expect "transferred"
+send "exit\n"
+interact
