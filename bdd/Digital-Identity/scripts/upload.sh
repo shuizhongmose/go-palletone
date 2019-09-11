edf4cbff51a6acfa9105dd0f0b79015201b46f84
shuizhongmose <602554102@qq.com>
2019-09-11 17:10:47 +0800

    delete useless codes

--- bdd/Digital-Identity/scripts/upload.sh
+++ bdd/Digital-Identity/scripts/upload.sh
@@ -4,4 +4,4 @@ cd /drone/src/github.com/palletone/go-palletone
 cp bdd/Digital-Identity/node/log/all.log $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
 cp bdd/Digital-Identity/node/nohup.out $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
 ls $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
-./bdd/uploadDrone2Ftp.sh ${FTP_PWD}
+./bdd/uploadDrone2Ftp.sh $FTP_PWD
