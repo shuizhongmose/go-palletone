533c4f7fd332adf0b4ebde25b5dfa0e2706afc7c
shuizhongmose <602554102@qq.com>
2019-09-12 14:30:51 +0800

    fix upload error

--- bdd/Digital-Identity/scripts/upload.sh
+++ bdd/Digital-Identity/scripts/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /drone/src/github.com/palletone/go-palletone
+cd $GOPATH/src/github.com/palletone/go-palletone
 cp bdd/Digital-Identity/node/log/all.log $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
 cp bdd/Digital-Identity/node/nohup.out $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
 ls $BDD_LOG_PATH/$DIGITAL_IDENTITY_DIR
