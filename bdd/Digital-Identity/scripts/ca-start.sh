e0263eb74871ef4e94eefb3f58f17b322b2bb01c
shuizhongmose <602554102@qq.com>
2019-09-16 13:34:29 +0800

    fix

--- bdd/Digital-Identity/scripts/ca-start.sh
+++ bdd/Digital-Identity/scripts/ca-start.sh
@@ -60,4 +60,3 @@ fabric-ca-client enroll --tls.client.certfile ca-cert.pem -u http://lk:123@local
 serial=$(openssl x509 -in ca-cert.pem -serial -noout | cut -d "=" -f 2)
 aki=$(openssl x509 -in ca-cert.pem -text | awk '/keyid/ {gsub(/ *keyid:|:/,"",$1);print tolower($0)}')
 fabric-ca-client revoke -s $serial -a $aki -r affiliationchange -u http://lk:123@localhost:7054 -M ~/cawork/root/msp -c ~/cawork/root/fabric-ca-server-config.yaml --gencrl
-
