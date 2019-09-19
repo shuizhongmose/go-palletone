64a4055ecdab9a6fd6c92f7bb1256140b3953378
shuizhongmose <602554102@qq.com>
2019-09-19 11:37:22 +0800

    test

--- cmd/deployment/modifyconfig.sh
+++ cmd/deployment/modifyconfig.sh
@@ -77,7 +77,8 @@ createaccount=`./createaccount.sh`
 tempinfo=`echo $createaccount | sed -n '$p'| awk '{print $NF}'`
 #accountlength=35
 #accounttemp=${tempinfo:0:$accountlength}
-account=`echo ${tempinfo///}`
+account=`echo ${tempinfo//
+/}`
 
 
 newAddress="Address=\"$account\""
@@ -94,14 +95,16 @@ key=`echo $info`
 privatekeylength=44
 private=${key#*private key: }
 privatekeytemp=${private:0:$privatekeylength}
-privatekey=`echo ${privatekeytemp///}`
+privatekey=`echo ${privatekeytemp//
+/}`
 #echo $privatekey
 
 
 publickeylength=175
 public=${key#*public key: }
 publickeytemp=${public:0:$publickeylength}
-publickey=`echo ${publickeytemp///}`
+publickey=`echo ${publickeytemp//
+/}`
 #echo $publickey
 
 
@@ -119,7 +122,8 @@ info=`./gptn nodeInfo`
 tempinfo=`echo $info | sed -n '$p'| awk '{print $NF}'`
 length=`echo ${#tempinfo}`
 nodeinfotemp=${tempinfo:0:$length}
-nodeinfo=`echo ${nodeinfotemp///}`
+nodeinfo=`echo ${nodeinfotemp//
+/}`
 length=`echo ${#nodeinfo}`
 b=140
 if [ "$length" -lt "$b" ]
@@ -157,6 +161,7 @@ function addBootstrapNodes()
 	if [ $acount -ne $index ];then
 	    #echo $acount
 	    nodeinfo=`echo $content | jq ".initialMediatorCandidates[ $[$acount-1] ].node"`
+	    echo "nodeinfo:"+$nodeinfo
 	    array="$array$nodeinfo,"
         fi
 	let ++acount;
