937538365d02bc47cf8eb4d06e9324a11409a458
shuizhongmose <602554102@qq.com>
2019-09-19 14:46:03 +0800

    fix spawn error

--- cmd/deployment/createaccount.sh
+++ cmd/deployment/createaccount.sh
@@ -1,14 +1,10 @@
-##!/usr/bin/expect
+#!/usr/bin/expect
 #!/bin/bash
-#set timeout 30
-#spawn ./gptn account new
-#expect "Passphrase:"
-#send "1\n"
-#expect "Repeat passphrase:"
-#send "1\n"
-#interact
+set timeout 30
+spawn ./gptn account new
+expect "Passphrase:"
+send "1\n"
+expect "Repeat passphrase:"
+send "1\n"
+interact
 
-./gptn account new << EOF
-1
-1
-EOF
--- cmd/deployment/init.sh
+++ cmd/deployment/init.sh
@@ -1,11 +1,9 @@
-##!/usr/bin/expect
-#!/bin/bash
-#set timeout 30
-#spawn ./gptn init
-#expect "Passphrase:"
-#send "1\n"
-#interact
+#!/usr/bin/expect
 
-./gptn init << EOF
-1
-EOF
+set timeout 30
+spawn ./gptn init
+expect "Passphrase:"
+send "1\n"
+interact
+
+
