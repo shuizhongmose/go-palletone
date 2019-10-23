987035a0b6c5b18a5b61941ee19ec150f91c1fb1
shuizhongmose <602554102@qq.com>
2019-10-23 17:20:24 +0800

    no message

--- bdd/dct/createaccount.sh
+++ bdd/dct/createaccount.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/expect
-#!/bin/bash
 set timeout 30
 spawn ../node/gptn account new
 expect "Passphrase:"
-send "1\r"
+send "1\n"
 expect "Repeat passphrase:"
-send "1\r"
+send "1\n"
 interact
 
