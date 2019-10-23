2157a964b64e075fdec05ceebeb179419e4040e2
shuizhongmose <602554102@qq.com>
2019-10-23 18:07:08 +0800

    fix

--- bdd/dct/transfertoken.sh
+++ bdd/dct/transfertoken.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/expect
+##!/usr/bin/expect
 #!/bin/bash
 set timeout 30
 set tokenHolder [lindex $argv 0]
 set another [lindex $argv 1]
-spawn ../node/gptn --exec "wallet.transferPTN($tokenHolder,$another,10000,1)" attach ../node/palletone/gptn.ipc
-expect "Passphrase:"
-send "1\n"
-interact
+#spawn ../node/gptn --exec "wallet.transferPTN($tokenHolder,$another,10000,1)" attach ../node/palletone/gptn.ipc
+#expect "Passphrase:"
+#send "1\n"
+#interact
+
+../node/gptn --exec "wallet.transferPTN($tokenHolder,$another,10000,1)" attach ../node/palletone/gptn.ipc << EOF
+1
+EOF
